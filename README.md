0e23516f7909b7510f71a87eaa8e75d5ec39d7d2
Andrea Rossini <rossini.andrea.lc@gmail.com>
2023-08-30 22:27:48 +0200

    14 Final render, at last!

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Raytracing in One Weekend
 
+I like to learn new languages, expecially the modern safe alternatives to native languages. For this project i used Zig and followed the Ray Tracing in One Weekend book. This gave me good insights, I might expand the project by importing stuff from my Joytracer, left stuck at implementing materials after poking around with blending hacks.
+
 ## References
 
 [_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const stdout = @import("stdio.zig").stdout;
+const random = @import("random.zig");
 const math = std.math;
 const vec3 = @import("vec3.zig");
 const raytracer = @import("raytracer.zig");
@@ -9,60 +10,90 @@ const Material = @import("material.zig").Material;
 const Sphere = raytracer.Sphere;
 const Color = vec3.Color;
 const Vec3 = vec3.Vec3;
+const Point3 = vec3.Point3;
 
 pub fn main() !void {
     const material_ground = Material.initLambertian(Color.init(
-        0.8,
-        0.8,
-        0.0,
-    ));
-    const material_center = Material.initLambertian(Color.init(
-        0.1,
-        0.2,
+        0.5,
+        0.5,
         0.5,
     ));
-    const material_left = Material.initDielectric(
-        1.5,
-    );
-    const material_right = Material.initMetal(
+    const material1 = Material.initDielectric(1.5);
+    const material2 = Material.initLambertian(Color.init(
+        0.4,
+        0.2,
+        0.1,
+    ));
+    const material3 = Material.initMetal(
         Color.init(
-            0.8,
+            0.7,
             0.6,
-            0.2,
+            0.5,
         ),
         0.0,
     );
-    const world = [_]raytracer.Hittable{
-        raytracer.Hittable{ .sphere = Sphere.init(
-            vec3.Point3.init(0, -100.5, -1),
-            100,
-            &material_ground,
-        ) },
-        raytracer.Hittable{ .sphere = Sphere.init(
-            vec3.Point3.init(0, 0, -1),
-            0.5,
-            &material_center,
-        ) },
-        raytracer.Hittable{ .sphere = Sphere.init(
-            vec3.Point3.init(-1, 0, -1),
-            0.5,
-            &material_left,
-        ) },
-        raytracer.Hittable{ .sphere = Sphere.init(
-            vec3.Point3.init(-1, 0, -1),
-            -0.4,
-            &material_left,
-        ) },
-        raytracer.Hittable{ .sphere = Sphere.init(
-            vec3.Point3.init(1, 0, -1),
-            0.5,
-            &material_right,
-        ) },
+    var world: [22 * 22 + 4]raytracer.Hittable = undefined;
+    var materialcache: [22 * 22]Material = undefined;
+    var idx: usize = 0;
+
+    var a: i32 = -11;
+    while (a < 11) : (a += 1) {
+        var b: i32 = -11;
+
+        while (b < 11) : (b += 1) {
+            const center = Point3.init(
+                @as(f64, @floatFromInt(a)) +
+                    0.9 * random.rand(),
+                0.2,
+                @as(f64, @floatFromInt(b)) +
+                    0.9 * random.rand(),
+            );
+
+            if (center.sub(Point3.init(4, 0.2, 0)).length() > 0.9) {
+                const choose_mat = random.rand();
+
+                if (choose_mat < 0.8) {
+                    const albedo = Color.product(
+                        Color.initRandom(),
+                        Color.initRandom(),
+                    );
+                    materialcache[idx] = Material.initLambertian(albedo);
+                } else if (choose_mat < 0.95) {
+                    const albedo = Color.initRandomRanged(0.5, 1);
+                    const fuzz = random.ranged(0, 0.5);
+                    materialcache[idx] = Material.initMetal(
+                        albedo,
+                        fuzz,
+                    );
+                } else {
+                    materialcache[idx] = Material.initDielectric(1.5);
+                }
+
+                world[idx] = raytracer.Hittable{
+                    .sphere = Sphere.init(center, 0.2, &materialcache[idx]),
+                };
+                idx = idx + 1;
+            }
+        }
+    }
+
+    world[idx] = raytracer.Hittable{
+        .sphere = Sphere.init(Point3.init(0, -1000, 0), 1000, &material_ground),
     };
+    world[idx + 1] = raytracer.Hittable{
+        .sphere = Sphere.init(Point3.init(0, 1, 0), 1.0, &material1),
+    };
+    world[idx + 2] = raytracer.Hittable{
+        .sphere = Sphere.init(Point3.init(-4, 1, 0), 1.0, &material2),
+    };
+    world[idx + 3] = raytracer.Hittable{
+        .sphere = Sphere.init(Point3.init(4, 1, 0), 1.0, &material3),
+    };
+    idx = idx + 4;
 
     const aspect_ratio = 16.0 / 9.0;
-    const image_width: u32 = 400;
-    const samples_per_pixel = 100;
+    const image_width: u32 = 1200;
+    const samples_per_pixel = 500;
     var camera = Camera.init(
         aspect_ratio,
         image_width,
@@ -71,13 +102,13 @@ pub fn main() !void {
     camera.max_depth = 50;
     camera.vfov = math.degreesToRadians(f64, 20);
     camera.lookAt(
-        Vec3.init(-2, 2, 1),
-        Vec3.init(0, 0, -1),
+        Vec3.init(13, 2, 3),
+        Vec3.init(0, 0, 0),
         Vec3.init(0, 1, 0),
-        3.4,
-        math.degreesToRadians(f64, 10),
+        10,
+        math.degreesToRadians(f64, 0.6),
     );
-    try camera.render(&world);
+    try camera.render(world[0..idx]);
 
     try stdout.context.flush(); // don't forget to flush!
 }
